2957142f861273737725e5417a279ba6ea9e75a6
kulahad <kulahad@users.noreply.github.com>
2020-12-14 17:43:20 +1300

    Add user dtos

---
+++ api/src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  password: string;
+}
---
+++ api/src/users/dto/update-user.dto.ts
@@ -0,0 +1,9 @@
+export class UpdateUserDto {
+  id: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  password: string;
+}
