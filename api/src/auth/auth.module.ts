428468fdabecad7fee3392b6ffccae08d41fa3d2
Mohammed Ahad <kulahad007@gmail.com>
2020-12-15 13:58:00 +1200

    Add jwt to API

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from '../users/users.service';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { jwtConstants } from './constants';
+import { PassportModule } from '@nestjs/passport';
+
 
 
 @Module({
-  imports: [UsersModule,
+  imports: [
+    UsersModule,
+    PassportModule,
     JwtModule.register({
-      secret: 's12356789'
-    })],
-  providers: [AuthService],
+      secret: jwtConstants.secret,
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
