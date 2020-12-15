0c9c1c878002de765cb142c204aa2200e8aad9b1
kulahad <kulahad@users.noreply.github.com>
2020-12-15 19:45:46 +1300

    Add public routes

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { Public } from './auth.guard';
 import { AuthService } from './auth.service';
 import { userAuth } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Public()
   @Post('login')
   async login(@Body() userAuth: userAuth): Promise<any> {
     return this.authService.login(userAuth);
--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto, UpdateUserDto } from './dto';
+import { Public } from 'src/auth/auth.guard';
 
 
 
@@ -8,6 +9,7 @@ import { CreateUserDto, UpdateUserDto } from './dto';
 export class UsersController {
   constructor(private userService: UsersService){};
 
+  @Public()
   @Post('register')
   async registerUser(@Body() user:CreateUserDto){
       return this.userService.registerUser(user);
