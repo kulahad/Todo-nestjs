921ca04fe0c90a58e81e788c4b870b4966c96d00
kulahad <kulahad@users.noreply.github.com>
2020-12-16 01:49:31 +1300

    test: add tests to task controller

--- api/src/tasks/tasks.controller.spec.ts
+++ api/src/tasks/tasks.controller.spec.ts
@@ -1,12 +1,35 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
+const Task = {
+  id: 'some-id',
+  title: 'Testing',
+  description: 'some-desc',
+  isComplete: true,
+};
 describe('TasksController', () => {
   let controller: TasksController;
+  const TaskModel = {
+    save: jest.fn().mockResolvedValue(Task),
+    find: jest.fn().mockResolvedValue([Task]),
+    findOne: jest.fn().mockResolvedValue(Task),
+    findByIdAndUpdate: jest.fn().mockResolvedValue(Task),
+    findById: jest.fn().mockResolvedValue(Task),
+    deleteOne: jest.fn().mockResolvedValue(true),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController],
+      providers: [
+        TasksController,
+        TasksService,
+        {
+          provide: getModelToken('Task'),
+          useValue: TaskModel,
+        },
+      ],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
