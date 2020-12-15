078a86b594c523b175c65e83e1b65e9ebf4ffd4f
Mohammed Ahad <kulahad007@gmail.com>
2020-12-15 13:58:52 +1200

    Remove unused imports

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { userAuth, UserDetails } from './dto';
+import { userAuth } from './dto';
 
 @Controller('auth')
 export class AuthController {
